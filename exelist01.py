c2bd95e77488da181a952238b8ca869896aa0d11
Marcus Vinicius <studymvsm@gmail.com>
2025-10-08 07:38:55 -0300

    Atualizando

--- exelist01.py
+++ exelist01.py
@@ -1,6 +1,6 @@
 # Exercise 1: Perform Basic List Operations
 my_list = [10, 20, 30, 40, 50]
-""" 
+
 print(my_list)
 print(f"Third item: {my_list[2]}")
 print(f"Length of t he list: {len(my_list)}")
@@ -45,10 +45,36 @@ largest = max(data)
 smallest = min(data)
 print(f"Largest number: {largest}")
 print(f"Smallest number: {smallest}")
-"""
+
 # Exercise 7: Count Occurrences
 sports = ['Cricket', 'Football', 'Hockey', 'Football', 'Tennis']
 football_count = sports.count('Football')
 print(f"Count: {football_count}")
 
+# Exercise 8: Sort a list of numbers
+numbers = [5, 2, 8, 1, 9]
+print(f"Original list: {numbers}")
+numbers.sort()
+print(f"Sorted list: {numbers}")
+
+# Exercise 9: Create a copy of a list
+list1 = [10,20,30]
+list2 = list1.copy() # ou list1[:]
+list1.append(40)
+print(list1)
+print(list2)
+
+# Exercise 10: Combine two lists
+list_a = [1, 2]
+list_b = [3, 4]
+
+list_plus = list_a + list_b
+print(f"{list_plus}")
+
+temp_list = [1,2]
+temp_list.extend(list_b)
+print(f"{temp_list}")
+
+unpacking_list = [*list_a,*list_b]
+print(f"{unpacking_list}")
 
---
+++ exelist02.py
@@ -0,0 +1,5 @@
+# Exercise 11: Remove empty strings from the list of strings
+list1 = ["Mike","","Emma","","Kelly","Brad"]
+
+result = list(filter(None,list1))
+print(result)
